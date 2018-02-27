240e78ccdbf5b1ea7b8e2b60c407db457d4af7b2
cyc1e <petroprom76@gmail.com>
2018-02-27 22:25:43 +0300

    js in mak done

--- mak/js/common.js
+++ mak/js/common.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-	$("p").animated("fadeIn")
+	$(".remark p").animated("fadeIn")
+	$(".middle_p p").animated("fadeIn")
+	$(".middle p").animated("fadeIn")
+	$(".after_middle p").animated("fadeIn")
+	$(".middle2 p").animated("fadeIn")
+	$(".new_features3 p").animated("fadeIn")
 	$(".insaid img").animated("fadeInUp");
 	});
